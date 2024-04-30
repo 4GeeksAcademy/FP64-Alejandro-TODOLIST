4d3e21924ce4473f695a3fa7c1ba7fabc67c3f02
alejo0022 <156907082+alejo0022@users.noreply.github.com>
2024-04-30 11:05:19 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,76 @@
-import React from "react";
+import React,{ useEffect, useState } from 'react';
+import './App.css';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function App() {
+  const [inputstate, setInputState] = useState('');
+  const [listaOriginal, setListaOriginal] = useState([]);
+  const [listaFiltrada, setListaFiltrada] = useState([]);
+  const [newInputText, setNewInputText] = useState(''); 
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  useEffect(() => {
+    setListaOriginal([
+      { name: 'Jordi' },
+      { name: 'Mauro' },
+      { name: 'Alejandro' },
+    ]);
+  }, []);
 
-export default Home;
+  useEffect(() => {
+    if (inputstate === '') {
+      setListaFiltrada(listaOriginal);
+    } else {
+      const estadoActualizado = listaOriginal.filter((item) => {
+        return item.name.toLowerCase().includes(inputstate.toLowerCase());
+      });
+      setListaFiltrada(estadoActualizado);
+    }
+  }, [listaOriginal, inputstate]);
+
+  const agregarNuevoMiembro = () => {
+    setListaOriginal([...listaOriginal, { name: newInputText }]);
+    setNewInputText('');
+  };
+
+  const eliminarMiembro = (index) => {
+    const nuevaLista = listaOriginal.filter((_, i) => i !== index);
+    setListaOriginal(nuevaLista);
+  };
+
+  return (
+    <>
+    <div className="container">
+      <div className="input">
+        <input
+          type="text"
+          value={newInputText}
+          onChange={(e) => setNewInputText(e.target.value)}
+        />
+        <button onClick={agregarNuevoMiembro}>Agregar</button>
+      </div>
+      <div className="input">
+        <input
+          type="text"
+          value={inputstate}
+          onChange={(e) => setInputState(e.target.value)}
+        ></input>
+        {inputstate === '' && <span> Filtra aqui</span>}
+      </div>
+      <div className="lista">
+      <ul>
+        {listaFiltrada.map((miembro, index) => {
+          return (
+            <li key={index}>
+              {miembro.name}
+              <button className="eliminar" onClick={() => eliminarMiembro(index)}>Eliminar</button>
+            </li>
+          );
+        })}
+      </ul>
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
